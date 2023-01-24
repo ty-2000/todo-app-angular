957fd395994740ffbe7f9b1efb617d1fa1086060
Takeaki <takeaki.yamamto@gmail.com>
2023-01-24 20:59:16 +0900

    エラーハンドリング

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Category } from 'src/models/category';
 
 import { Todo } from 'src/models/todo';
@@ -18,9 +19,28 @@ export class TodoService {
 
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.todosUrl)
+      .pipe(
+        tap(todos => console.log('fetched todos')),
+        catchError(this.handleError<Todo[]>('getHeroes', []))
+      );
   }
 
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.categoriesUrl)
+      .pipe(
+        tap(categories => console.log('fetched categories')),
+        catchError(this.handleError<Category[]>('getHeroes', []))
+      );
+}
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: リモート上のロギング基盤にエラーを送信する
+      console.error(error); // かわりにconsoleに出力
+  
+      // 空の結果を返して、アプリを持続可能にする
+      return of(result as T);
+    };
   }
 }
