f4c2dd8d90a7dd94d40c31303d7b98bb3ce8e8ca
Takeaki <takeaki.yamamto@gmail.com>
2023-01-31 17:49:30 +0900

    unsubscribeの実装

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, EventEmitter } from '@angular/core';
 
 import { Todo } from 'src/models/todo';
 import { Category } from 'src/models/category';
 import { TodoService } from '../todo.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy {
+  private readonly onDestroy$ = new EventEmitter();
 
   todoList: { todo: Todo, category: Category }[] = []
 
   getTodoList(): void {
-    this.todoService.getTodos().subscribe(todos => 
-      this.todoService.getCategories().subscribe(categories => 
-        this.todoList = todos.map(todo => {
-          const category = categories.find(category => todo.category_id == category.id)
-          if (category == null) { throw new Error('category is not defined')}
-          return { todo: todo, category: category }
-        })))
+    this.todoService.getTodos()
+      .pipe(takeUntil(this.onDestroy$),)
+      .subscribe(todos => 
+        this.todoService.getCategories()
+          .pipe(takeUntil(this.onDestroy$),)
+          .subscribe(categories => 
+            this.todoList = todos.map(todo => {
+              const category = categories.find(category => todo.category_id == category.id)
+              if (category == null) { throw new Error('category is not defined')}
+              return { todo: todo, category: category }
+            })))
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.emit()
   }
 
   constructor(
