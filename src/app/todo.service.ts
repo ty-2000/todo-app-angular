970d3226f21bb8ea866d66afb9f4088398347ea0
Takeaki <takeaki.yamamto@gmail.com>
2023-02-02 09:10:30 +0900

    dbに追加するサービスを実装

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -16,6 +16,9 @@ export class TodoService {
 
   private todosUrl = 'api/todos';
   private categoriesUrl = 'api/categories';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.todosUrl)
@@ -31,7 +34,15 @@ export class TodoService {
         tap(categories => console.log('fetched categories')),
         catchError(this.handleError<Category[]>('getCategories', []))
       );
-}
+  }
+
+  addTodo(todo: Todo): Observable<Todo>{
+    return this.http.post<Todo>(this.todosUrl, todo, this.httpOptions)
+      .pipe(
+        tap((newTodo: Todo) => console.log(`added hero w/ id=${newTodo.id}`)),
+        catchError(this.handleError<Todo>('addTodo'))
+      )
+  }
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
